b1c6615af7e6beffe815bba778687a2160276e5f
Neil Hu <hysh1991@gmail.com>
2020-07-17 23:46:08 +1000

    login mutation  error handler

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 import AuthForm from "./AuthForm";
+import { graphql } from "react-apollo";
+
+import mutation from "../mutations/login";
+import query from "../queries/CurrentUser";
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { errors: [] };
+  }
+
+  onSubmit({ email, password }) {
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }],
+      })
+      .catch((res) => {
+        const errors = res.graphQLErrors.map((error) => error.message);
+        this.setState({ errors });
+      });
+  }
+
   render() {
     return (
       <div>
         <h4>Login</h4>
-        <AuthForm />
+        <AuthForm
+          onSubmit={this.onSubmit.bind(this)}
+          errors={this.state.errors}
+        />
       </div>
     );
   }
 }
 
-export default LoginForm;
+export default graphql(mutation)(LoginForm);
