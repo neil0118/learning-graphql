07f53e5f3a1be9370ed4deac76b5d58b2d6a31ed
Neil Hu <hysh1991@gmail.com>
2020-07-18 12:01:17 +1000

    signup form

---
+++ client/components/SignupForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import AuthForm from "./AuthForm";
+import { graphql } from "react-apollo";
+
+import mutation from "../mutations/signup";
+import query from "../queries/CurrentUser";
+
+class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
+  onSubmit({ email, password }) {
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }],
+      })
+      .catch((res) => {
+        const errors = res.graphQLErrors.map((error) => error.message);
+        this.setState({ errors });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Sign Up</h4>
+        <AuthForm
+          onSubmit={this.onSubmit.bind(this)}
+          errors={this.state.errors}
+        />
+      </div>
+    );
+  }
+}
+
+export default graphql(mutation)(SignupForm);
